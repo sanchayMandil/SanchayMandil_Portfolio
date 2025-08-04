66c02005640d1dcee0b34bfec75e784e0d08a154
sanchayMandil <sanchaymandil@gmail.com>
2025-08-04 07:01:03 +0530

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -34,7 +34,7 @@ function Intro() {
   return (
     <section className="px-4 sm:px-[5vw] min-h-[80vh]">
       <div className="mx-auto flex flex-col md:flex-row items-center justify-between gap-8 md:gap-0">
-        <div className="text-center md:text-left w-full md:w-3/5 space-y-7">
+        <div className="text-center md:text-left w-full md:w-[70%] space-y-7">
           <h1 className="text-4xl sm:text-5xl lg:text-6xl font-extrabold my-3 leading-tight bg-gradient-to-r from-[#EBD3F8] via-white to-[#EBD3F8] bg-clip-text text-transparent drop-shadow-lg">
             Trust me, I am a{" "}
             <span className="text-[#EBD3F8] italic drop-shadow-md">
@@ -51,14 +51,14 @@ function Intro() {
             <span className="text-[#EBD3F8] font-bold underline underline-offset-4">MITS</span>.
           </p>
         </div>
-        <div className="w-full md:w-2/5 flex justify-center mt-8 md:mt-0">
+        <div className="w-full md:w-[30%] flex justify-center mt-8 md:mt-0">
           <motion.div
             className="relative group"
             style={{ width: "18rem", height: "18rem", maxWidth: "100%", maxHeight: "100%" }}
           >
             {/* Profile Image */}
             <img
-              className="relative h-70 sm:h-64 md:h-70 w-70 sm:w-64 md:w-100 object-cover rounded-full border-8 border-transparent shadow-2xl transition-transform duration-300 group-hover:scale-105"
+              className="relative h-70 sm:h-64 md:h-75 w-70 sm:w-64 md:w-100 object-cover rounded-full border-8 border-transparent shadow-2xl transition-transform duration-300 group-hover:scale-105"
               src="https://cbx-prod.b-cdn.net/COLOURBOX58688312.jpg?width=800&height=800&quality=70"
               alt="Software Engineer Profile Picture"
               style={{ zIndex: 2 }}
